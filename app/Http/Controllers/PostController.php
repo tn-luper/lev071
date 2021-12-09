04a9c3069d1ba1b5d709f89ebae751a129382b09
tn-luper <tatsuki.nakategawa@gmail.com>
2021-12-09 15:54:09 +0900

    削除機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,4 +41,11 @@ class PostController extends Controller
 
     return redirect('/posts/' . $post->id);
     }
+    
+    public function destroy(Post $post)
+    {
+    $post->delete();
+    return redirect('/');
+        
+    }
 }
