b5a9ee35c45bfb81fb8a8842bfb963b4a8f5ac1b
tn-luper <tatsuki.nakategawa@gmail.com>
2021-12-09 12:57:15 +0900

    ブログ投稿編集機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,4 +28,17 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function edit(Post $post)
+    {
+    return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+    }
 }
